2d1d55d38019f6503f86ba2880d0e8e30989167c
rtdevcraft <hiiamabat@gmail.com>
2025-05-06 11:29:48 -0600

    Testing minimal page.tsx
    Seeing what this does to build?

--- validation-form/src/app/page.tsx
+++ validation-form/src/app/page.tsx
@@ -1,9 +1,18 @@
-import ContactForm from './components/ContactForm'
+// import ContactForm from './components/ContactForm'
 
-export default function Home() {
+// export default function Home() {
+//   return (
+//     <main className='flex flex-col items-center justify-between p-24'>
+//       <ContactForm />
+//     </main>
+//   )
+// }
+
+export default function HomePage() {
   return (
-    <main className='flex flex-col items-center justify-between p-24'>
-      <ContactForm />
-    </main>
+    <div>
+      <h1>Minimal Test Page on Vercel</h1>
+      <p>If you see this, the basic page serving works!</p>
+    </div>
   )
 }
