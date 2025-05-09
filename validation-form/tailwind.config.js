c5ea80bb6bb8f929d708d834483594f811694b4e
rtdevcraft <hiiamabat@gmail.com>
2025-05-09 16:26:01 -0600

    Update to tailwind.config.js
    Added back the safelist as that was working, small tradeoff for slightly larger css file.

--- validation-form/tailwind.config.js
+++ validation-form/tailwind.config.js
@@ -1,3 +1,22 @@
-module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-}
+safelist: [
+    // Colors for the "down" state (label as placeholder)
+    'peer-placeholder-shown:text-black',
+    'peer-placeholder-shown:text-red-500',
+
+    // Colors for the "up" state (label focused or input has value)
+    'peer-focus:text-white',
+    'peer-[:not(:placeholder-shown)]:text-white',
+
+    // And the positional/size classes for the "up" state, just to be sure
+    'peer-focus:top-[-1rem]',
+    'peer-focus:-translate-y-0',
+    'peer-focus:text-xs',
+    'peer-[:not(:placeholder-shown)]:top-[-1rem]',
+    'peer-[:not(:placeholder-shown)]:-translate-y-0',
+    'peer-[:not(:placeholder-shown)]:text-xs',
+
+    // Base positional/size classes for the "down" state
+    'peer-placeholder-shown:top-1/2',
+    'peer-placeholder-shown:-translate-y-1/2',
+    'peer-placeholder-shown:text-base',
+  ],
