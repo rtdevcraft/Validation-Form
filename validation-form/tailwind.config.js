17b30ffccf3268a4cb4cb5b7372004667f1fb614
rtdevcraft <hiiamabat@gmail.com>
2025-05-09 16:18:23 -0600

    testing tailwind config
    simplifying tailwind config without safelist to see if it is still purging on build

--- validation-form/tailwind.config.js
+++ validation-form/tailwind.config.js
@@ -1,22 +1,3 @@
 module.exports = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  safelist: [
-    'text-white',
-    'peer-focus:text-white',
-    'peer-[:not(:placeholder-shown)]:text-white',
-    'text-black',
-    'text-red-500',
-    'peer-placeholder-shown:text-black',
-    'peer-placeholder-shown:text-red-500',
-    'peer-focus:top-[-1rem]',
-    'peer-focus:-translate-y-0',
-    'peer-focus:text-xs',
-    'peer-[:not(:placeholder-shown)]:top-[-1rem]',
-    'peer-[:not(:placeholder-shown)]:-translate-y-0',
-    'peer-[:not(:placeholder-shown)]:text-xs',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
 }
