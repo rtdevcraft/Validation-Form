1c67acf2a372478593d88cba91339f6fa51c8519
rtdevcraft <hiiamabat@gmail.com>
2025-05-09 16:09:18 -0600

    Testing tailwind config
    Seeing if the build is purging needed tailwind classes

--- validation-form/tailwind.config.js
+++ validation-form/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    // Add other paths if necessary
+  ],
+  safelist: [
+    'text-white',
+    'peer-focus:text-white',
+    'peer-[:not(:placeholder-shown)]:text-white',
+    'text-black',
+    'text-red-500',
+    'peer-placeholder-shown:text-black',
+    'peer-placeholder-shown:text-red-500',
+    'peer-focus:top-[-1rem]',
+    'peer-focus:-translate-y-0',
+    'peer-focus:text-xs',
+    'peer-[:not(:placeholder-shown)]:top-[-1rem]',
+    'peer-[:not(:placeholder-shown)]:-translate-y-0',
+    'peer-[:not(:placeholder-shown)]:text-xs',
   ],
 }
